459b1a3a542a7d6f94cf6aaaf020f738f274fb7a
Emilin4t0r <virolainen.emil@gmail.com>
2020-09-07 16:27:33 +0300

    part 3 and 4 of assignment 2.

--- Assignments/Assign2/Program.cs
+++ Assignments/Assign2/Program.cs
@@ -19,21 +19,81 @@ namespace Assign2
                     InstantiatePlayers(1000000);
                     break;
                 case "2":
-                    CreatePlayerWithItems(3);
+                    Player plr = CreatePlayerWithItems(3);
+                    PrintLevelsOfAllItems(plr);
+                    Console.WriteLine("Highest level item of player " + plr.Id + " is item " + plr.GetHighestLevelItem().Id);
+                    break;
+                case "3":
+                    Console.WriteLine("\nWithout LINQ:");
+                    Item[] itemArray = GetItems(CreatePlayerWithItems(3));
+                    for (int i = 0; i < itemArray.Length; i++)
+                    {
+                        Console.WriteLine("item number " + i + " ID in array: " + itemArray[i].Id);
+                    }
+                    Console.WriteLine("\nWith LINQ: ");
+                    itemArray = GetItemsWithLinq(CreatePlayerWithItems(3));
+                    for (int i = 0; i < itemArray.Length; i++)
+                    {
+                        Console.WriteLine("item number " + i + " ID in array: " + itemArray[i].Id);
+                    }
+                    break;
+                case "4":
+                    Console.WriteLine("\nWithout LINQ:");
+                    Console.WriteLine("First item in the list: " + FirstItem(CreatePlayerWithItems(3)).Id);
+                    Console.WriteLine("\nWith LINQ: ");
+                    Console.WriteLine("First item in the list: " + FirstItemWithLinq(CreatePlayerWithItems(3)).Id);
                     break;
             }
         }
 
-        static void CreatePlayerWithItems(int amount)
+        static Item[] GetItems(Player player)
+        {
+            Item[] items = new Item[player.Items.Count];
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = player.Items[i];
+            }
+            return items;
+        }
+
+        static Item[] GetItemsWithLinq(Player player)
+        {
+            Item[] items = player.Items.ToArray();
+            return items;
+        }
+
+        static Item FirstItem(Player player)
+        {
+            Item[] items = player.Items.ToArray();
+            if (items[0] != null)
+            {
+                return items[0];
+            }
+            return null;
+        }
+
+        static Item FirstItemWithLinq(Player player)
+        {
+            return player.Items.FirstOrDefault();
+        }
+
+        static Player CreatePlayerWithItems(int amount)
         {
             Player playerWithItems = new Player(Guid.NewGuid(), 0, new List<Item>());
             Random rnd = new Random();
             for (int i = 0; i < amount; i++)
             {
                 playerWithItems.Items.Add(new Item(Guid.NewGuid(), rnd.Next(1, 101)));
-                Console.WriteLine(playerWithItems.Items[i].Id + " level is " + playerWithItems.Items[i].Level);
             }
-            Console.WriteLine("Highest level item of player " + playerWithItems.Id + " is item " + playerWithItems.GetHighestLevelItem().Id);
+            return playerWithItems;
+        }
+
+        static void PrintLevelsOfAllItems(Player player)
+        {
+            for (int i = 0; i < player.Items.Count; i++)
+            {
+                Console.WriteLine(player.Items[i].Id + " level is " + player.Items[i].Level);
+            }
         }
 
         static void InstantiatePlayers(int amount)
